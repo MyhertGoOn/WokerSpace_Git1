b4890a5d495b1da33b409c18d74947311c2f77d9
yusen <kele@senyuhuang>
2021-08-07 17:50:15 +0800

    master first commit

--- src/main/java/sen/yuhuang/test/helloWorld.java
+++ src/main/java/sen/yuhuang/test/helloWorld.java
@@ -6,5 +6,6 @@ public class helloWorld {
         System.out.println("hello World222");
         System.out.println("hello World333");
         System.out.println("这是hot-fix修改的代码");
+        System.out.println("这是演示合并冲突master修改的代码");
     }
 }
