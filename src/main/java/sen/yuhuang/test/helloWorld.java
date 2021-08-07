0de941478f4f0147aab5870323d77ba8a681f905
MyhertGoOn <88566190+MyhertGoOn@users.noreply.github.com>
2021-08-07 18:21:06 +0800

    Update helloWorld.java

--- src/main/java/sen/yuhuang/test/helloWorld.java
+++ src/main/java/sen/yuhuang/test/helloWorld.java
@@ -8,5 +8,7 @@ public class helloWorld {
         System.out.println("这是hot-fix修改的代码");
         System.out.println("这是演示合并冲突的hot-fix修改的代码");
         System.out.println("我为了演示push,修改了本地代码");
+        System.out.println("我为了演示pull,修改了远程库的代码");
+        
     }
 }
