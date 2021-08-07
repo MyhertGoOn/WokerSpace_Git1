8bd4ae574e26d9866570933ed7e5776c02c51a3e
yusen <kele@senyuhuang>
2021-08-07 16:56:56 +0800

    secondly commit

--- src/main/java/sen/yuhuang/test/helloWorld.java
+++ src/main/java/sen/yuhuang/test/helloWorld.java
@@ -3,5 +3,6 @@ package sen.yuhuang.test;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("hello World");
+        System.out.println("hello World222");
     }
 }
